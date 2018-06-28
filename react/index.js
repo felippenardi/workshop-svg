d8c82db7257ed331b2ca6a248a316a03cd347995
Felippe Nardi <felippe@nardi.me>
2018-06-28 19:54:07 -0300

    Add example swiper with progress tracker

--- react/index.js
+++ react/index.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import SwipeableViews from 'react-swipeable-views';
 
-export default function Welcome () {
-  return (
-    <div>
-      <h1>Welcome</h1>
-    </div>
-  )
+const styles = {
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    background: '#FEA900',
+  },
+  slide2: {
+    background: '#B3DC4A',
+  },
+  slide3: {
+    background: '#6AC0FF',
+  },
+};
+
+export default class Welcome extends Component {
+  state = {
+    progress: 0
+  }
+  handleSwitching = (index, type) => {
+    const progress = Math.trunc(index * 100) % 100
+    this.setState({ progress })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{ this.state.progress }%</h1>
+        <SwipeableViews
+          enableMouseEvents={true}
+          onSwitching={this.handleSwitching}
+        >
+          <div style={Object.assign({}, styles.slide, styles.slide1)}>
+            slide n°1
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide2)}>
+            slide n°2
+          </div>
+          <div style={Object.assign({}, styles.slide, styles.slide3)}>
+            slide n°3
+          </div>
+        </SwipeableViews>
+      </div>
+    )
+  }
 }
